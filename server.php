248d5ec28d2cba7afbc2605cc2738df9a4c03a91
lance21312 <151377477+lance21312@users.noreply.github.com>
2024-09-18 14:19:31 -0400

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['inst'] = "https://inv.nadeko.net/api/v1";
+$GLOBALS['inst'] = "https://invidious.darkness.services/api/v1";
 function invidiousGet($url) {
 	return json_decode(file_get_contents($GLOBALS['inst'] . $url), true);
 }
